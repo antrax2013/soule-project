aba770feaec347a884a7ed4c584b45a92fa08c43
cyrilcophignon <cyrilcophignon@yahoo.fr>
2012-07-21 07:38:03 +0000

    creation de la version de dev v0.2

--- SR V0.1/structure/footer/content-footer.php
+++
@@ -1,5 +0,0 @@
-<div id="Footer" class="ui-widget ui-widget-header">
-	<a href="<?php echo $rootPath?>mentions-legales.php">Mentions légales</a>
-	<a href="<?php echo $rootPath?>index.php" title="soule.royale.free.fr"><img id="icone" src="<?php echo $image?>soule-royale-min.png" alt="<?php echo $siteName?>" title="<?php echo $siteName?>"  /></a>
-	<a href="<?php echo $rootPath?>site-map.php" title="Soule royale: le plan du site">Plan du site</a>
-</div>
